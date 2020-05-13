2db8d8e781f486dce973ee175742187be6a02223
Buzzlez <tom-hornberger@gmx.de>
2020-05-13 22:04:39 +0000

    Aufgabe 1c

--- poissont.cc
+++ poissont.cc
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    double uk = pow(mu, k);
+    double eu = exp(-mu);
+    double f = tgamma(k+1);
+
+    double p = uk*eu/f;
+
+
+    return p;
 }
 
 int main() {
@@ -13,8 +21,10 @@ int main() {
 
     ifstream fin("datensumme.txt");
     ofstream fout("hist.txt");
+    ofstream fout2("histpoi.txt");
     int n_i;
     vector<int> zaehler(11);
+    
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
         zaehler[n_i] +=1;
@@ -22,15 +32,17 @@ int main() {
     }
     fin.close();
     int m = 0;
+    double mu = 3.11538;
+    int h = 234;
     for(std::vector<int>::const_iterator n = zaehler.begin(); n != zaehler.end(); ++n){
-      
-      std::cout << *n << ' ';
+      double poi = poisson(mu, m);
+      std::cout << *n << endl;
 
       fout << m << ' ' << *n << endl;
+      fout2 << m << ' ' << *n << ' ' << h*poi << endl;
       ++m;
     }
 
-    
     
 
 }

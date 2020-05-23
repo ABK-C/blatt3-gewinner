9c210ad032e4b393bcac54df50dca488065f9f6c
repl.it user <replituser@example.com>
2020-05-23 13:07:51 +0000

    Williams Aufgabe 2 a) - d)

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,62 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
-double poisson(double mu, int k) {
-    return 0;
+double prob(int k, double mu) {
+return pow(mu,k)*exp(-mu)/tgamma(k+1);    
 }
+
+double mu;
+double L = 1;
 
 int main() {
     using namespace std;
 
 
     ifstream fin("datensumme.txt");
-    int n_i;
+    ofstream fout("likelihood.txt");
+    ofstream fout2("nll.txt");
+    ofstream fout3("deltanll.txt");
+
+    vector<int> daten;
+    int n_i, zahl;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        daten.push_back(n_i);
     }
+
+    for(int k : daten) {
+    double li = prob(k, 3.11538);
+    L *= li;
+    }
+
+    cout << L << endl;
+
+
+    for (mu = 0; mu < 6; mu+=0.01) {
+      double L2 = 1;
+
+      for(int k : daten) {
+        double li2 = prob(k, mu);
+        L2 *= li2;
+      }
+
+      fout << mu << " " << L2 << endl;
+      
+      fout2 << mu << " " << -2*log(L2) << endl;
+
+      fout3 << mu << " " << -2*log(L2)-(-2*log(3.11538)) << endl;
+      
+    }
+    fout << "Der beste Schätzwert für mu liegt bei ca. 3.12." << endl;
+    fout3 << "Das Minimum des negative Log-Likelihood -ln(L) liegt ebenso bei mu = 3.12 und beträgt 446,955. Das Intervall, in dem -ln(L) jm weniger als 1,0 größer als im Minimum ist, beträgt [2.96:3.28]" << endl;
+
     fin.close();
+    fout.close();
+    fout2.close();
+    fout3.close();
+
+
 }
